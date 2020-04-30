36fff5256283179db9ae57d325c61786488e7775
Jeroen Stevens <jeroenstevens@gmail.com>
2020-04-30 16:58:57 -0600

    Add spring boot app and use maven to build and test

---
+++ src/main/java/com/app/test/TestController.java
@@ -0,0 +1,15 @@
+package com.app.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class TestController {
+
+
+    @GetMapping("/")
+    public String test() {
+        return "OK";
+    }
+
+}
